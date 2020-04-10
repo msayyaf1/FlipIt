21d4df909fcbd1d923fba855cddc8a5942d819e3
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-04-10 17:37:55 +0530

    Created custom classes

---
+++ Flipit/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flipit
+//
+//  Created by Mohamed Sayyaf on 10/04/20.
+//  Copyright © 2020 Mohamed Sayyaf. All rights reserved.
+//
+
+import Foundation
+
+class Card{ //Class containing
+    var imageName = ""
+    var isFlipped = false
+    var isMatched = false
+    
+}
---
+++ Flipit/CardModel.swift
@@ -0,0 +1,42 @@
+//
+//  CardModel.swift
+//  Flipit
+//
+//  Created by Mohamed Sayyaf on 10/04/20.
+//  Copyright © 2020 Mohamed Sayyaf. All rights reserved.
+//
+
+import Foundation
+
+class CardModel { // model class for cards
+    
+    func getCards() -> [Card] {
+        
+        //Declare an array to store generated cards
+        var generatedCardsArray = [Card]()
+        
+        // randomly generate pairs of cards
+        for _ in 1...8 {
+            // Generating random numbers
+            let randomNumber = arc4random_uniform(13) + 1
+            
+            // Create the first card object
+            let cardOne = Card()  // initializing a card object
+            cardOne.imageName = "card\(randomNumber)"
+            
+            generatedCardsArray.append(cardOne)
+            
+            // Create the second card object
+            let cardTwo = Card() // initializing another card object
+            cardTwo.imageName = "card\(randomNumber)"
+            
+            generatedCardsArray.append(cardTwo)
+            
+            // OPTIONAL: Implement unique pair of cards
+        }
+        // TODO: Randomize the array
+        
+        //return the array
+        return generatedCardsArray
+    }
+}
