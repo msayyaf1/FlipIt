1a824feb044efcf0aa794f2b843dd4191e91a146
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-04-11 14:13:23 +0530

    custom classes and iboutlets done

---
+++ Flipit/CardCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CardCollectionViewCell.swift
+//  Flipit
+//
+//  Created by Mohamed Sayyaf on 11/04/20.
+//  Copyright © 2020 Mohamed Sayyaf. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    // This is the subclass denoting the collection view's cell
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+}
