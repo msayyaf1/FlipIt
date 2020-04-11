3ef60900be68c653f38348ff110faff44544fa7d
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-04-11 22:41:37 +0530

    collection view iboutlet and protocol methods

--- Flipit/ViewController.swift
+++ Flipit/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     var model = CardModel() // creating a new object of the cardmodel class
     var cardArray = [Card]()
     
     
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // call the getCards method of the card model
-        cardArray = model.getCards() 
+        cardArray = model.getCards()
+        
+        //setting the view controller as the delegate (dispatcher) for both protocols
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    // MARK: - UICollectionView protocol methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return cardArray.count   // this method is the requires method of datasource protocol
+                                 // like the nineohtwo method defined inside dispatcher class in example
+        // this function returns the number of cards generated my the model
     }
 
 
