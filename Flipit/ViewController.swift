aa1dbd489cb86171cdbfdc5f1347ebc270407c28
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-04-11 22:57:36 +0530

    cards generated

--- Flipit/ViewController.swift
+++ Flipit/ViewController.swift
@@ -35,7 +35,32 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
                                  // like the nineohtwo method defined inside dispatcher class in example
         // this function returns the number of cards generated my the model
     }
+    
+    // when the collection view asks the viewcontroller for new data to be displayed
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // returns a uicollectionview object
+        //indexpath defines which cell the collection view is asking for
+        
+        //the method below tries to get a cell to resuse, maybe a one that is scrolled out of view
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    // next is protocol method for uicollectoinviewdelegate to capture user interaction with the cards
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
 
 
 }
+
+//  MARK: - SO HOW IT WORKS
+
+// in the CardModel get cards method we randomly generated 8 pairs of cards,
+// go to the view controller, the collection view asks for the no of items, we return cardarray.count,   therefore the cellforitem at protocol method will be called 16 times
 
